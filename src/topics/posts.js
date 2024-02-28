90ec235783f45b97ac2e428736374e7f1c5a70c7
Forest Ma <forestm@andrew.cmu.edu>
2024-02-28 14:36:30 -0500

    revert to accepted answer pop up commit

--- src/topics/posts.js
+++ src/topics/posts.js
@@ -167,9 +167,7 @@ module.exports = function (Topics) {
                 post.display_edit_tools = topicPrivileges.isAdminOrMod || (post.selfPost && topicPrivileges['posts:edit']);
                 post.display_delete_tools = topicPrivileges.isAdminOrMod || (post.selfPost && topicPrivileges['posts:delete']);
                 post.display_moderator_tools = post.display_edit_tools || post.display_delete_tools;
-                post.hide_accept_button = post.display_moderator_tools || post.topicOwnerPost || !post.isTopicOwner;
-                post.display_accept_button = !post.display_moderator_tools && !post.topicOwnerPost && post.isTopicOwner && !post.accepted;
-                post.display_unaccept_button = !post.display_moderator_tools && !post.topicOwnerPost && post.isTopicOwner && post.accepted;
+                post.display_accept_button = !post.display_moderator_tools && !post.topicOwnerPost && post.isTopicOwner;
                 post.display_move_tools = topicPrivileges.isAdminOrMod && post.index !== 0;
                 post.display_post_menu = topicPrivileges.isAdminOrMod ||
                     (post.selfPost &&
