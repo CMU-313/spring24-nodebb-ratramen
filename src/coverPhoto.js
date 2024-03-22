b3910a16c2b778dd2dd7732015275e416b3110a8
Bruno Neira <bneira@andrew.cmu.edu>
2024-03-21 21:56:45 -0400

    relative path to camel case

--- src/coverPhoto.js
+++ src/coverPhoto.js
@@ -3,7 +3,7 @@
 const nconf = require('nconf')
 const meta = require('./meta')
 
-const relative_path = nconf.get('relative_path')
+const relativePath = nconf.get('relative_path')
 
 const coverPhoto = module.exports
 
@@ -16,7 +16,7 @@ coverPhoto.getDefaultProfileCover = function (uid) {
 }
 
 function getCover (type, id) {
-  const defaultCover = `${relative_path}/assets/images/cover-default.png`
+  const defaultCover = `${relativePath}/assets/images/cover-default.png`
   if (meta.config[`${type}:defaultCovers`]) {
     const covers = String(meta.config[`${type}:defaultCovers`]).trim().split(/[\s,]+/g)
     let coverPhoto = defaultCover
@@ -30,7 +30,7 @@ function getCover (type, id) {
       id %= covers.length
     }
     if (covers[id]) {
-      coverPhoto = covers[id].startsWith('http') ? covers[id] : (relative_path + covers[id])
+      coverPhoto = covers[id].startsWith('http') ? covers[id] : (relativePath + covers[id])
     }
     return coverPhoto
   }
